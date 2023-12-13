bb512065d4ed1f25504f630043f7b34f1e9141e6
Danila Dombrovskiy <daniladombr@mail.ru>
2023-12-13 10:42:35 +0400

    fix gui

--- gui/gui.py
+++ gui/gui.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, messagebox
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\6blct\Downloads\Tkinter-Designer-master\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r".\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
