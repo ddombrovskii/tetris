c98ce9f8617caaaacecafadf50d3ffdecdf7ace0
Danila Dombrovsky <44140463+ddombrovskii@users.noreply.github.com>
2023-12-27 06:29:40 +0400

    Delete temp

--- temp.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect('server.db')
-sql = db.cursor()
-for value in sql.execute("SELECT login, password, raiting FROM users"):
-    login, pas, raiting = list(value)
-    print(login, pas, raiting)
